7bd7e590e23fc1894914198de5cca96aecc84fca
Umra07 <vol.karpenko07@gmail.com>
2023-04-11 23:39:25 +0200

    fixes

--- src/components/Header/SecondaryNav/style.ts
+++ src/components/Header/SecondaryNav/style.ts
@@ -1,7 +1,20 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export const SecondaryNavWrapper = styled.div`
   display: flex;
   align-items: center;
   gap: 15px;
+`;
+
+export const DropdownBtn = styled.button`
+  &:hover {
+    svg {
+      transform: rotate(180deg);
+    }
+  }
+
+  svg {
+    enable-background: new 0 0 512 512;
+    transition: transform 367ms cubic-bezier(0.21, 0, 0.07, 1);
+  }
 `;
