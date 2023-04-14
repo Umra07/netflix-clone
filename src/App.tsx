006dd2dd2759aaa30e4744f36ea8c3f1d9478518
Umra07 <vol.karpenko07@gmail.com>
2023-04-14 17:06:56 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from './components/Header';
+import Invite from './components/Invite';
 import { AppContainer } from './styles/AppContainer.styled';
 
 function App() {
   return (
     <AppContainer>
       <Header />
+      <Invite />
     </AppContainer>
   );
 }
---
+++ src/components/Invite/index.tsx
@@ -0,0 +1,8 @@
+import { InviteSection } from './style';
+
+const Invite = () => {
+  console.log(import.meta.env);
+  return <InviteSection>Invite</InviteSection>;
+};
+
+export default Invite;
